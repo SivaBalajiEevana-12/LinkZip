7abde12c4bd95026f73170c31f648a1cb2588847
SivaBalajiEevana-12 <eevanasivabalaji@gmail.com>
2026-09-12 12:29:04 +0530

    build

---
+++ server/src/routes/user.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const router = express.Router();
+const { registerUser, loginUser, refreshToken, logoutUser, getMe } = require("../controller/user");
+const authMiddleware = require("../middleware/auth");
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refresh", refreshToken);
+router.post("/logout", authMiddleware, logoutUser);
+router.get("/me", authMiddleware, getMe);
+module.exports = router;
+//# sourceMappingURL=user.js.map
