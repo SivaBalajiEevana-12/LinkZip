ac78e3771a6bed34d04e5fdb2a653cc519b2edce
SivaBalajiEevana-12 <eevanasivabalaji@gmail.com>
2026-09-15 11:25:00 +0530

    version 1.2

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ db().then(()=>{
 })
 app.get('/', async (req:any,res:any)=>{
     console.log("/get");
-    const users= {"message”:"welcome to link zip"};
+    const users= {message:"welcome to link zip"}
 
     return res.json(users);
 });
